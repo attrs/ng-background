57773cfa67a2e2b75def578d219d74a0c55fe884
joje <joje.attrs@gmail.com>
2017-02-08 02:41:15 +0900

    add ng-background-repeat

--- ng-background.js
+++ ng-background.js
@@ -8,13 +8,15 @@ module.exports = angular.module('ngBackground', [])
           element.css({
             'background-image': 'url(' + img +')',
             'background-size': attrs.ngBackgroundSize || 'cover',
-            'background-position': attrs.ngBackgroundPosition || 'center'
+            'background-position': attrs.ngBackgroundPosition || 'center',
+            'background-repeat': attrs.ngBackgroundRepeat || 'no-repeat'
           });
         } else {
           element.css({
             'background-image': '',
             'background-size': '',
-            'background-position': ''
+            'background-position': '',
+            'background-repeat': ''
           });
         }
       };
